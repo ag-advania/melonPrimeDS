63a9f3cd10b5d6b45ab1b291fe2c2426909fd280
Antoine Giraud <antoine.giraud@advania.is>
2024-09-24 13:29:18 +0000

    test??

--- src/frontend/qt_sdl/EmuThread.cpp
+++ src/frontend/qt_sdl/EmuThread.cpp
@@ -1210,44 +1210,8 @@ void EmuThread::run()
                 // inGame
 
                 
-                // OSD Testing
-                Top_paint->setPen(Qt::white);
-                Top_paint->setRenderHint(QPainter::TextAntialiasing, false);
-                QFont font2(family, 8);
-                Top_paint->setFont(font2);
-                Top_paint->drawText(QPoint(14, 188), (std::string("Size 8 ABCDEFGHI_+- ")));
-
-                QFont font3(family, 7);
-                Top_paint->setFont(font3);
-                Top_paint->drawText(QPoint(24, 188), (std::string("Size 7 ABCDEFGHI_+- ")));
-
-                QFont font4(family, 6);
-                Top_paint->setFont(font4);
-                Top_paint->drawText(QPoint(34, 188), (std::string("Size 6 ABCDEFGHI_+- ")));
-
-                QFont font5(family, 5);
-                Top_paint->setFont(font5);
-                Top_paint->drawText(QPoint(34, 188), (std::string("Size 5 ABCDEFGHI_+- ")));
-
-                QFont font6(family, 9);
-                Top_paint->setFont(font6);
-                Top_paint->drawText(QPoint(44, 188), (std::string("Size 9 ABCDEFGHI_+- ")));
-
-                QFont font7(family, 10);
-                Top_paint->setFont(font7);
-                Top_paint->drawText(QPoint(64, 188), (std::string("Size 10 ABCDEFGHI_+- ")));
-
-                QFont font8(family, 11);
-                Top_paint->setFont(font8);
-                Top_paint->drawText(QPoint(94, 188), (std::string("Size 11 ABCDEFGHI_+- ")));
-
-                QFont font9(family, 12);
-                Top_paint->setFont(font9);
-                Top_paint->drawText(QPoint(114, 188), (std::string("Size 12 ABCDEFGHI_+- ")));
-                Top_paint->drawText(QPoint(4, 188), (std::string("HP ") + std::to_string(currentHP)).c_str());
-
-
-
+        
+                
 
 
                 // OSD testing end
@@ -1333,6 +1297,40 @@ void EmuThread::run()
                     Top_paint->drawText(QPoint(164, 178), (std::string("Other Ammo: ") + std::to_string(ammoConsumption)).c_str());
                 }
 
+                // OSD Testing
+                Top_paint->setPen(Qt::white);
+                Top_paint->setRenderHint(QPainter::TextAntialiasing, false);
+                QFont font2(family, 8);
+                Top_paint->setFont(font2);
+                Top_paint->drawText(QPoint(14, 188), (std::string("Size 8 ABCDEFGHI_+- ")));
+
+                QFont font3(family, 7);
+                Top_paint->setFont(font3);
+                Top_paint->drawText(QPoint(24, 188), (std::string("Size 7 ABCDEFGHI_+- ")));
+
+                QFont font4(family, 6);
+                Top_paint->setFont(font4);
+                Top_paint->drawText(QPoint(34, 188), (std::string("Size 6 ABCDEFGHI_+- ")));
+
+                QFont font5(family, 5);
+                Top_paint->setFont(font5);
+                Top_paint->drawText(QPoint(34, 188), (std::string("Size 5 ABCDEFGHI_+- ")));
+
+                QFont font6(family, 9);
+                Top_paint->setFont(font6);
+                Top_paint->drawText(QPoint(44, 188), (std::string("Size 9 ABCDEFGHI_+- ")));
+
+                QFont font7(family, 10);
+                Top_paint->setFont(font7);
+                Top_paint->drawText(QPoint(64, 188), (std::string("Size 10 ABCDEFGHI_+- ")));
+
+                QFont font8(family, 11);
+                Top_paint->setFont(font8);
+                Top_paint->drawText(QPoint(94, 188), (std::string("Size 11 ABCDEFGHI_+- ")));
+
+                QFont font9(family, 12);
+                Top_paint->setFont(font9);
+                Top_paint->drawText(QPoint(114, 188), (std::string("Size 12 ABCDEFGHI_+- ")));
 
 
                 // Draw Crosshair:
