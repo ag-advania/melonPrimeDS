a1164fdfe2e03ee387417c5d516212cea2cd276b
Zection6V <zection6vmph@gmail.com>
2024-11-23 16:47:55 +0900

    reverted

--- src/frontend/qt_sdl/EmuThread.cpp
+++ src/frontend/qt_sdl/EmuThread.cpp
@@ -1833,14 +1833,6 @@ void EmuThread::run()
 			else {
                 // VirtualStylus
 
-                // Touch Scren
-                if (Input::HotkeyDown(HK_MetroidShootScan) || Input::HotkeyDown(HK_MetroidScanShoot)) {
-                    NDS->TouchScreen(virtualStylusX, virtualStylusY);
-                }
-                else {
-                    NDS->ReleaseScreen();
-                }
-
                 if (!OSD) {
                     OSD = mainWindow->panel->OSDCanvas;
                     Top_buffer = OSD[0].CanvasBuffer;
@@ -1893,6 +1885,14 @@ void EmuThread::run()
                 // Draw VirtualStylus : 3x3 center Crosshair
                 Btm_paint->drawLine(virtualStylusX - 1, virtualStylusY, virtualStylusX + 1, virtualStylusY);
                 Btm_paint->drawLine(virtualStylusX, virtualStylusY - 1, virtualStylusX, virtualStylusY + 1);
+
+                // Touch Scren
+                if (Input::HotkeyDown(HK_MetroidShootScan) || Input::HotkeyDown(HK_MetroidScanShoot)) {
+                    NDS->TouchScreen(virtualStylusX, virtualStylusY);
+                }
+                else {
+                    NDS->ReleaseScreen();
+                }
 
 			} 
 
