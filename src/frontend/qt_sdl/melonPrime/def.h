7b25032c03671f1093c990cd020c18cd64dcd8d3
Zection6V <zection6vmph@gmail.com>
2024-11-28 23:52:43 +0900

    Fixed

--- src/frontend/qt_sdl/melonPrime/def.h
+++ src/frontend/qt_sdl/melonPrime/def.h
@@ -1,27 +1,6 @@
-/*
-    Copyright 2016-2023 melonDS team
-
-    This file is part of melonDS.
-
-    melonDS is free software: you can redistribute it and/or modify it under
-    the terms of the GNU General Public License as published by the Free
-    Software Foundation, either version 3 of the License, or (at your option)
-    any later version.
-
-    melonDS is distributed in the hope that it will be useful, but WITHOUT ANY
-    WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-    FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License along
-    with melonDS. If not, see http://www.gnu.org/licenses/.
-*/
-
 #ifndef DEF_H
 #define DEF_H
 
-
-#include <QTimer>
-#include <cstdint> // added for romversions
 
 // Declare global variable with extern (to make it accessible from other files)
 extern uint32_t globalChecksum;
