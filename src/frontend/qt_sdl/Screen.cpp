887a8fc75a074b57576326176de67cae22680a77
Zection6V <zection6vmph@gmail.com>
2026-02-05 23:01:58 +0900

    add glFinish

--- src/frontend/qt_sdl/Screen.cpp
+++ src/frontend/qt_sdl/Screen.cpp
@@ -33,10 +33,8 @@
 
 // MelonPrimeDS Integration
 #include "MelonPrime.h"
-#ifdef _WIN32
-#include <dwmapi.h>
-#pragma comment(lib, "dwmapi.lib")
-#endif
+
+
 
 // melonprimeds
 #if defined(_WIN32)
@@ -1367,12 +1365,8 @@ void ScreenPanelGL::drawScreen()
 
     glContext->SwapBuffers();
 
-    // ▼▼▼ 追加: DWMのバッファフラッシュを待機 ▼▼▼
-#ifdef _WIN32
-    // これにより、DWMが画面を更新するまでCPUがブロックされます
-    // 実質的に「レンダリング前フレーム」がDWMの管理下に置かれます
-    DwmFlush();
-#endif
+    glFinish(); // MelonPrimeDS
+
 }
 
 qreal ScreenPanelGL::devicePixelRatioFromScreen() const
