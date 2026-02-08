84476d7f4650961c6653af99b904d554649558ea
Zection6V <zection6vmph@gmail.com>
2026-02-09 02:02:14 +0900

    Refactoring config dialogs

--- src/frontend/qt_sdl/InputConfig/InputConfigDialog.h
+++ src/frontend/qt_sdl/InputConfig/InputConfigDialog.h
@@ -26,6 +26,10 @@
 #include "Config.h"
 #include "EmuInstance.h"
 
+#ifdef MELONPRIME_DS
+#include "MelonPrimeInputConfig.h"
+#endif
+
 static constexpr int keypad_num = 12;
 
 static constexpr std::initializer_list<int> hk_addons =
@@ -94,95 +98,6 @@ static constexpr std::initializer_list<const char*> hk_general_labels =
 
 static_assert(hk_general.size() == hk_general_labels.size());
 
-#ifdef MELONPRIME_DS
-// MelonPrimeDS {
-static constexpr std::initializer_list<int> hk_tabAddonsMetroid =
-{
-    HK_MetroidMoveForward,
-    HK_MetroidMoveBack,
-    HK_MetroidMoveLeft,
-    HK_MetroidMoveRight,
-    HK_MetroidShootScan,
-    HK_MetroidScanShoot,
-    HK_MetroidZoom,
-    HK_MetroidJump,
-    HK_MetroidMorphBall,
-    HK_MetroidHoldMorphBallBoost,
-    HK_MetroidWeaponBeam,
-    HK_MetroidWeaponMissile,
-    HK_MetroidWeapon1,
-    HK_MetroidWeapon2,
-    HK_MetroidWeapon3,
-    HK_MetroidWeapon4,
-    HK_MetroidWeapon5,
-    HK_MetroidWeapon6,
-    HK_MetroidWeaponSpecial,
-    HK_MetroidMenu,
-
-};
-
-static constexpr std::initializer_list<const char*> hk_tabAddonsMetroid_labels =
-{
-    "[Metroid] (W) Move Forward",
-    "[Metroid] (S) Move Back",
-    "[Metroid] (A) Move Left",
-    "[Metroid] (D) Move Right",
-    "[Metroid] (Mouse Left) Shoot/Scan, Map Zoom In",
-    "[Metroid] (V) Scan/Shoot, Map Zoom In",
-    "[Metroid] (Mouse Right) Imperialist Zoom, Map Zoom Out, Morph Ball Boost",
-    "[Metroid] (Space) Jump",
-    "[Metroid] (L. Ctrl) Transform",
-    "[Metroid] (Shift) Hold to Fast Morph Ball Boost",
-    "[Metroid] (Mouse 5, Side Top) Weapon Beam",
-    "[Metroid] (Mouse 4, Side Bottom) Weapon Missile",
-    "[Metroid] (1) Weapon 1. ShockCoil",
-    "[Metroid] (2) Weapon 2. Magmaul",
-    "[Metroid] (3) Weapon 3. Judicator",
-    "[Metroid] (4) Weapon 4. Imperialist",
-    "[Metroid] (5) Weapon 5. Battlehammer",
-    "[Metroid] (6) Weapon 6. VoltDriver",
-    "[Metroid] (R) Affinity Weapon (Last used Weapon/Omega cannon)",
-    "[Metroid] (Tab) Menu/Map",
-};
-
-static_assert(hk_tabAddonsMetroid.size() == hk_tabAddonsMetroid_labels.size());
-
-
-static constexpr std::initializer_list<int> hk_tabAddonsMetroid2 =
-{
-    HK_MetroidIngameSensiUp,
-    HK_MetroidIngameSensiDown,
-    HK_MetroidWeaponNext,
-    HK_MetroidWeaponPrevious,
-    HK_MetroidScanVisor,
-    HK_MetroidUILeft,
-    HK_MetroidUIRight,
-    HK_MetroidUIOk,
-    HK_MetroidUIYes,
-    HK_MetroidUINo,
-    HK_MetroidWeaponCheck,
-};
-
-static constexpr std::initializer_list<const char*> hk_tabAddonsMetroid2_labels =
-{
-
-    "[Metroid] (PgUp) AimSensitivity Up",
-    "[Metroid] (PgDown) AimSensitivity Down",
-    "[Metroid] (J) Next Weapon in the sorted order",
-    "[Metroid] (K) Previous Weapon in the sorted order",
-    "[Metroid] (C) Scan Visor",
-    "[Metroid] (Z) UI Left (Adventure Left Arrow / Hunter License L)",
-    "[Metroid] (X) UI Right (Adventure Right Arrow / Hunter License R)",
-    "[Metroid] (F) UI Ok",
-    "[Metroid] (G) UI Yes (Enter Starship)",
-    "[Metroid] (H) UI No (Enter Starship)",
-    "[Metroid] (Y) Weapon Check",
-};
-
-static_assert(hk_tabAddonsMetroid2.size() == hk_tabAddonsMetroid2_labels.size());
-// } MelonPrimeDS
-#endif
-
 namespace Ui { class InputConfigDialog; }
 class InputConfigDialog;
 
@@ -218,8 +133,8 @@ public:
 #ifdef MELONPRIME_DS
     /* MelonPrimeDS { */
     void switchTabToAddons();
-    void switchTabToMetroid(); // Tab Other Metroid Settings
-    void switchTabToMetroid2(); // Tab Other Metroid Settings 2
+    void switchTabToMetroid();
+    void switchTabToMetroid2();
     /* } MelonPrimeDS*/
 #endif
 
@@ -233,21 +148,6 @@ private slots:
 #endif
 
     void on_cbxJoystick_currentIndexChanged(int id);
-
-#ifdef MELONPRIME_DS
-    /* MelonPrimeDS { */
-    void on_metroidResetSensitivityValues_clicked();
-
-    void on_metroidSetVideoQualityToLow_clicked();
-    void on_metroidSetVideoQualityToHigh_clicked();
-    void on_metroidSetVideoQualityToHigh2_clicked();
-
-    void on_cbMetroidEnableSnapTap_stateChanged(int state);
-    void on_cbMetroidUnlockAll_stateChanged(int state);
-    void on_cbMetroidApplyHeadphone_stateChanged(int state);
-    void on_cbMetroidUseFirmwareName_stateChanged(int state);
-    /* } MelonPrimeDS*/
-#endif
 
 private:
     void populatePage(QWidget* page,
@@ -265,14 +165,13 @@ private:
     int keypadKeyMap[12], keypadJoyMap[12];
     int addonsKeyMap[hk_addons.size()], addonsJoyMap[hk_addons.size()];
 
-#ifdef MELONPRIME_DS
-    int addonsMetroidKeyMap[hk_tabAddonsMetroid.size()], addonsMetroidJoyMap[hk_tabAddonsMetroid.size()]; // MelonPrimeDS
-    int addonsMetroid2KeyMap[hk_tabAddonsMetroid2.size()], addonsMetroid2JoyMap[hk_tabAddonsMetroid2.size()]; // MelonPrimeDS
-#endif
-
     int hkGeneralKeyMap[hk_general.size()], hkGeneralJoyMap[hk_general.size()];
     int joystickID;
-};
 
+#ifdef MELONPRIME_DS
+    // ïœêîñºÇÕêÊì™è¨ï∂éöÇ…ÇµÇƒÉNÉâÉXñºÇ∆ãÊï Ç∑ÇÈ
+    MelonPrimeInputConfig* melonPrimeInputConfig;
+#endif
+};
 
 #endif // INPUTCONFIGDIALOG_H
