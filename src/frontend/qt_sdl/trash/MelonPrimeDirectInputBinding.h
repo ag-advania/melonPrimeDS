9487a85650a066a3e6f7480183907d914ee4457e
Zection6V <zection6vmph@gmail.com>
2026-04-15 08:32:38 +0900

    removing trash

--- src/frontend/qt_sdl/trash/MelonPrimeDirectInputBinding.h
+++
@@ -1,6 +0,0 @@
-// MelonPrimeDirectInputBinding.h
-#pragma once
-#ifdef _WIN32
-class MelonPrimeDirectInputFilter;
-void MelonPrime_BindMetroidHotkeysFromJoystickConfig(MelonPrimeDirectInputFilter* din, int instance);
-#endif
