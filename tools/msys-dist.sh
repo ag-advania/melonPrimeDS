2afe04fb1f6d86b74e9b8992ca1dc8ccb74c6b3f
Zection6V <zection6vmph@gmail.com>
2026-06-14 13:25:03 +0900

    updated exe name

--- tools/msys-dist.sh
+++ tools/msys-dist.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-if [[ ! -x melonDS.exe ]]; then
-	echo "Run this script from the directory you built melonDS."
+if [[ ! -x melonPrimeDS.exe ]]; then
+	echo "Run this script from the directory you built melonPrimeDS."
 	exit 1
 fi
 
 mkdir -p dist
 
-for lib in $(ldd melonDS.exe | grep mingw | sed "s/.*=> //" | sed "s/(.*)//"); do
+for lib in $(ldd melonPrimeDS.exe | grep mingw | sed "s/.*=> //" | sed "s/(.*)//"); do
 	cp "${lib}" dist
 done
 
-cp melonDS.exe dist
+cp melonPrimeDS.exe dist
 windeployqt dist
