e8c22f144047e5c9e4a5b95e03c5eefba7b30d33
Zection6V <zection6vmph@gmail.com>
2026-07-10 23:47:13 +0900

    Metal Compute Shader Phase 7I：MSL／初期化修正

--- .metal_compute_phase7i_backup/20260710_234438/src/GPU.cpp
+++ .metal_compute_phase7i_backup/20260710_234438/src/GPU.cpp
@@ -327,11 +327,6 @@ void GPU::SetRenderer(std::unique_ptr<Renderer>&& renderer) noexcept
         }
         else
         {
-#ifdef MELONPRIME_DS
-            // MELONPRIME_RENDERER_INIT_FALLBACK_LOG_V1
-            Platform::Log(Platform::LogLevel::Error,
-                "Renderer init failed; falling back to software renderer\n");
-#endif
             // TODO: report error to platform
         }
     }
