39db1f94bd6853b939577fac1e4d7917d3de4d23
Zection6V <zection6vmph@gmail.com>
2026-07-14 22:23:40 +0900

    remove backups

--- .melonprime_vulkan_v0_v5_backup/20260713_220531/files/src/frontend/qt_sdl/MelonPrimeVulkanToonHighlightBootstrap.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#if !defined(MELONPRIME_DS) || !defined(MELONPRIME_ENABLE_VULKAN)
-#error "MelonPrimeVulkanToonHighlightBootstrap is owned by the Vulkan build gate"
-#endif
-// MELONPRIME_VULKAN_TOON_HIGHLIGHT_BOOTSTRAP_V1
-// MELONPRIME_VULKAN_TOON_HIGHLIGHT_SHADER_ABI_BOOTSTRAP_V1
-class QString;
-namespace MelonPrime::Vulkan { int RunToonHighlightShaderAbiHarness(const QString& outputPath); }
