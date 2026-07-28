melonDS-emu/melonDS

commit 544fefa27f698f3a0d799a782dc03d3eb47561db

Refactor the JIT to be object-oriented (#1879)
* Move TinyVector to a new file

- So it's less sensitive to #include ordering

* Forgot to include assert.h

* Refactor ARMJIT_Memory into an object

* Oops, forgot a declaration

* Refactor ARMJIT to be contained in an object

* Remove an unused function declaration

* Add a missing #include

* Remove a now-unused global

* Use ARMJIT_Memory's own memory access functions

* Fix some omissions in the ARM JIT

* Move libandroid to be a member of ARMJIT_Memory instead of a global

* Default-initialize most fields in ARMJIT_Compiler.h

* Define NOOP_IF_NO_JIT

* Finish refactoring the JIT to be object-oriented

src/ARMJIT_Memory.h
@@ -20,62 +20,209 @@
 #define ARMJIT_MEMORY
 
 #include "types.h"
+#include "TinyVector.h"
 
 #include "ARM.h"
+#include "DSi.h"
 
-namespace ARMJIT_Memory
+#if defined(__SWITCH__)
+#include <switch.h>
+#elif defined(_WIN32)
+#include <windows.h>
+#else
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <signal.h>
+#endif
+
+#ifndef JIT_ENABLED
+#include <array>
+#include "NDS.h"
+#endif
+
+namespace ARMJIT
 {
-
-extern void* FastMem9Start;
-extern void* FastMem7Start;
-
-void Init();
-void DeInit();
-
-void Reset();
-
-enum
-{
-    memregion_Other = 0,
-    memregion_ITCM,
-    memregion_DTCM,
-    memregion_BIOS9,
-    memregion_MainRAM,
-    memregion_SharedWRAM,
-    memregion_IO9,
-    memregion_VRAM,
-    memregion_BIOS7,
-    memregion_WRAM7,
-    memregion_IO7,
-    memregion_Wifi,
-    memregion_VWRAM,
-
-    // DSi
-    memregion_BIOS9DSi,
-    memregion_BIOS7DSi,
-    memregion_NewSharedWRAM_A,
-    memregion_NewSharedWRAM_B,
-    memregion_NewSharedWRAM_C,
-
-    memregions_Count
-};
-
-int ClassifyAddress9(u32 addr);
-int ClassifyAddress7(u32 addr);
-
-bool GetMirrorLocation(int region, u32 num, u32 addr, u32& memoryOffset, u32& mirrorStart, u32& mirrorSize);
-u32 LocaliseAddress(int region, u32 num, u32 addr);
-
-bool IsFastmemCompatible(int region);
-
-void RemapDTCM(u32 newBase, u32 newSize);
-void RemapSWRAM();
-void RemapNWRAM(int num);
-
-void SetCodeProtection(int region, u32 offset, bool protect);
-
-void* GetFuncForAddr(ARM* cpu, u32 addr, bool store, int size);
-
+class Compiler;
+class ARMJIT;
 }
+
+constexpr u32 RoundUp(u32 size) noexcept
+{
+#ifdef _WIN32
+    return (size + 0xFFFF) & ~0xFFFF;
+#else
+    return size;
+#endif
+}
+
+const u32 MemBlockMainRAMOffset = 0;
+const u32 MemBlockSWRAMOffset = RoundUp(NDS::MainRAMMaxSize);
+const u32 MemBlockARM7WRAMOffset = MemBlockSWRAMOffset + RoundUp(NDS::SharedWRAMSize);
+const u32 MemBlockDTCMOffset = MemBlockARM7WRAMOffset + RoundUp(NDS::ARM7WRAMSize);
+const u32 MemBlockNWRAM_AOffset = MemBlockDTCMOffset + RoundUp(DTCMPhysicalSize);
+const u32 MemBlockNWRAM_BOffset = MemBlockNWRAM_AOffset + RoundUp(DSi::NWRAMSize);
+const u32 MemBlockNWRAM_COffset = MemBlockNWRAM_BOffset + RoundUp(DSi::NWRAMSize);
+const u32 MemoryTotalSize = MemBlockNWRAM_COffset + RoundUp(DSi::NWRAMSize);
+
+class ARMJIT_Memory
+{
+public:
+    enum
+    {
+        memregion_Other = 0,
+        memregion_ITCM,
+        memregion_DTCM,
+        memregion_BIOS9,
+        memregion_MainRAM,
+        memregion_SharedWRAM,
+        memregion_IO9,
+        memregion_VRAM,
+        memregion_BIOS7,
+        memregion_WRAM7,
+        memregion_IO7,
+        memregion_Wifi,
+        memregion_VWRAM,
+
+        // DSi
+        memregion_BIOS9DSi,
+        memregion_BIOS7DSi,
+        memregion_NewSharedWRAM_A,
+        memregion_NewSharedWRAM_B,
+        memregion_NewSharedWRAM_C,
+
+        memregions_Count
+    };
+
+#ifdef JIT_ENABLED
+public:
+    explicit ARMJIT_Memory(ARMJIT::ARMJIT& jit) noexcept;
+    ~ARMJIT_Memory() noexcept;
+    ARMJIT_Memory(const ARMJIT_Memory&) = delete;
+    ARMJIT_Memory(ARMJIT_Memory&&) = delete;
+    ARMJIT_Memory& operator=(const ARMJIT_Memory&) = delete;
+    ARMJIT_Memory& operator=(ARMJIT_Memory&&) = delete;
+    void Reset() noexcept;
+    void RemapDTCM(u32 newBase, u32 newSize) noexcept;
+    void RemapSWRAM() noexcept;
+    void RemapNWRAM(int num) noexcept;
+    void SetCodeProtection(int region, u32 offset, bool protect) noexcept;
+
+    [[nodiscard]] u8* GetMainRAM() noexcept { return MemoryBase + MemBlockMainRAMOffset; }
+    [[nodiscard]] const u8* GetMainRAM() const noexcept { return MemoryBase + MemBlockMainRAMOffset; }
+
+    [[nodiscard]] u8* GetSharedWRAM() noexcept { return MemoryBase + MemBlockSWRAMOffset; }
+    [[nodiscard]] const u8* GetSharedWRAM() const noexcept { return MemoryBase + MemBlockSWRAMOffset; }
+
+    [[nodiscard]] u8* GetARM7WRAM() noexcept { return MemoryBase + MemBlockARM7WRAMOffset; }
+    [[nodiscard]] const u8* GetARM7WRAM() const noexcept { return MemoryBase + MemBlockARM7WRAMOffset; }
+
+    [[nodiscard]] u8* GetARM9DTCM() noexcept { return MemoryBase + MemBlockDTCMOffset; }
+    [[nodiscard]] const u8* GetARM9DTCM() const noexcept { return MemoryBase + MemBlockDTCMOffset; }
+
+    [[nodiscard]] u8* GetNWRAM_A() noexcept { return MemoryBase + MemBlockNWRAM_AOffset; }
+    [[nodiscard]] const u8* GetNWRAM_A() const noexcept { return MemoryBase + MemBlockNWRAM_AOffset; }
+
+    [[nodiscard]] u8* GetNWRAM_B() noexcept { return MemoryBase + MemBlockNWRAM_BOffset; }
+    [[nodiscard]] const u8* GetNWRAM_B() const noexcept { return MemoryBase + MemBlockNWRAM_BOffset; }
+
+    [[nodiscard]] u8* GetNWRAM_C() noexcept { return MemoryBase + MemBlockNWRAM_COffset; }
+    [[nodiscard]] const u8* GetNWRAM_C() const noexcept { return MemoryBase + MemBlockNWRAM_COffset; }
+
+    int ClassifyAddress9(u32 addr) const noexcept;
+    int ClassifyAddress7(u32 addr) const noexcept;
+    bool GetMirrorLocation(int region, u32 num, u32 addr, u32& memoryOffset, u32& mirrorStart, u32& mirrorSize) const noexcept;
+    u32 LocaliseAddress(int region, u32 num, u32 addr) const noexcept;
+    bool IsFastmemCompatible(int region) const noexcept;
+    void* GetFuncForAddr(ARM* cpu, u32 addr, bool store, int size) const noexcept;
+    bool MapAtAddress(u32 addr) noexcept;
+private:
+    friend class ARMJIT::Compiler;
+    struct Mapping
+    {
+        u32 Addr;
+        u32 Size, LocalOffset;
+        u32 Num;
+
+        void Unmap(int region, ARMJIT_Memory& memory) noexcept;
+    };
+
+    struct FaultDescription
+    {
+        u32 EmulatedFaultAddr;
+        u8* FaultPC;
+    };
+    static bool FaultHandler(FaultDescription& faultDesc, ARMJIT::ARMJIT& jit);
+    bool MapIntoRange(u32 addr, u32 num, u32 offset, u32 size) noexcept;
+    bool UnmapFromRange(u32 addr, u32 num, u32 offset, u32 size) noexcept;
+    void SetCodeProtectionRange(u32 addr, u32 size, u32 num, int protection) noexcept;
+
+    ARMJIT::ARMJIT& JIT;
+    void* FastMem9Start;
+    void* FastMem7Start;
+    u8* MemoryBase = nullptr;
+#if defined(__SWITCH__)
+    VirtmemReservation* FastMem9Reservation, *FastMem7Reservation;
+    u8* MemoryBaseCodeMem;
+#elif defined(_WIN32)
+    static LONG ExceptionHandler(EXCEPTION_POINTERS* exceptionInfo);
+    HANDLE MemoryFile = INVALID_HANDLE_VALUE;
+    LPVOID ExceptionHandlerHandle = nullptr;
+#else
+    static void SigsegvHandler(int sig, siginfo_t* info, void* rawContext);
+    int MemoryFile = -1;
+#endif
+#ifdef ANDROID
+    Platform::DynamicLibrary* Libandroid = nullptr;
+#endif
+    u8 MappingStatus9[1 << (32-12)] {};
+    u8 MappingStatus7[1 << (32-12)] {};
+    ARMJIT::TinyVector<Mapping> Mappings[memregions_Count] {};
+#else
+public:
+    explicit ARMJIT_Memory(ARMJIT::ARMJIT&) {};
+    ~ARMJIT_Memory() = default;
+    ARMJIT_Memory(const ARMJIT_Memory&) = delete;
+    ARMJIT_Memory(ARMJIT_Memory&&) = delete;
+    ARMJIT_Memory& operator=(const ARMJIT_Memory&) = delete;
+    ARMJIT_Memory& operator=(ARMJIT_Memory&&) = delete;
+
+    void Reset() noexcept {}
+    void RemapDTCM(u32 newBase, u32 newSize) noexcept {}
+    void RemapSWRAM() noexcept {}
+    void RemapNWRAM(int num) noexcept {}
+    void SetCodeProtection(int region, u32 offset, bool protect) noexcept {}
+
+    [[nodiscard]] u8* GetMainRAM() noexcept { return MainRAM.data(); }
+    [[nodiscard]] const u8* GetMainRAM() const noexcept { return MainRAM.data(); }
+
+    [[nodiscard]] u8* GetSharedWRAM() noexcept { return SharedWRAM.data(); }
+    [[nodiscard]] const u8* GetSharedWRAM() const noexcept { return SharedWRAM.data(); }
+
+    [[nodiscard]] u8* GetARM7WRAM() noexcept { return ARM7WRAM.data(); }
+    [[nodiscard]] const u8* GetARM7WRAM() const noexcept { return ARM7WRAM.data(); }
+
+    [[nodiscard]] u8* GetARM9DTCM() noexcept { return DTCM.data(); }
+    [[nodiscard]] const u8* GetARM9DTCM() const noexcept { return DTCM.data(); }
+
+    [[nodiscard]] u8* GetNWRAM_A() noexcept { return NWRAM_A.data(); }
+    [[nodiscard]] const u8* GetNWRAM_A() const noexcept { return NWRAM_A.data(); }
+
+    [[nodiscard]] u8* GetNWRAM_B() noexcept { return NWRAM_B.data(); }
+    [[nodiscard]] const u8* GetNWRAM_B() const noexcept { return NWRAM_B.data(); }
+
+    [[nodiscard]] u8* GetNWRAM_C() noexcept { return NWRAM_C.data(); }
+    [[nodiscard]] const u8* GetNWRAM_C() const noexcept { return NWRAM_C.data(); }
+private:
+    std::array<u8, NDS::MainRAMMaxSize> MainRAM {};
+    std::array<u8, NDS::ARM7WRAMSize> ARM7WRAM {};
+    std::array<u8, NDS::SharedWRAMSize> SharedWRAM {};
+    std::array<u8, DTCMPhysicalSize> DTCM {};
+    std::array<u8, DSi::NWRAMSize> NWRAM_A {};
+    std::array<u8, DSi::NWRAMSize> NWRAM_B {};
+    std::array<u8, DSi::NWRAMSize> NWRAM_C {};
+#endif
+};
 
 #endif

src/ARM.cpp
@@ -26,11 +26,7 @@
 #include "ARMJIT.h"
 #include "Platform.h"
 #include "GPU.h"
-
-#ifdef JIT_ENABLED
-#include "ARMJIT.h"
 #include "ARMJIT_Memory.h"
-#endif
 
 using Platform::Log;
 using Platform::LogLevel;
@@ -88,7 +84,7 @@ void ARM::GdbCheckC() {}
 
 
 
-u32 ARM::ConditionTable[16] =
+const u32 ARM::ConditionTable[16] =
 {
     0xF0F0, // EQ
     0x0F0F, // NE
@@ -108,16 +104,14 @@ u32 ARM::ConditionTable[16] =
     0x0000  // NE
 };
 
-
-ARM::ARM(u32 num, Melon::GPU& gpu) :
+ARM::ARM(u32 num, ARMJIT::ARMJIT& jit, Melon::GPU& gpu) :
 #ifdef GDBSTUB_ENABLED
     GdbStub(this, Platform::GetConfigInt(num ? Platform::GdbPortARM7 : Platform::GdbPortARM9)),
 #endif
+    JIT(jit),
+    Num(num), // well uh
     GPU(gpu)
 {
-    // well uh
-    Num = num;
-
 #ifdef GDBSTUB_ENABLED
     if (Platform::GetConfigBool(Platform::GdbEnabled)
 #ifdef JIT_ENABLED
@@ -134,25 +128,21 @@ ARM::~ARM()
     // dorp
 }
 
-ARMv5::ARMv5(Melon::GPU& gpu) : ARM(0, gpu)
+ARMv5::ARMv5(ARMJIT::ARMJIT& jit, Melon::GPU& gpu) : ARM(0, jit, gpu)
 {
-#ifndef JIT_ENABLED
-    DTCM = new u8[DTCMPhysicalSize];
-#endif
+    DTCM = JIT.Memory.GetARM9DTCM();
 
     PU_Map = PU_PrivMap;
 }
 
-ARMv4::ARMv4(Melon::GPU& gpu) : ARM(1, gpu)
+ARMv4::ARMv4(ARMJIT::ARMJIT& jit, Melon::GPU& gpu) : ARM(1, jit, gpu)
 {
     //
 }
 
 ARMv5::~ARMv5()
 {
-#ifndef JIT_ENABLED
-    delete[] DTCM;
-#endif
+    // DTCM is owned by Memory, not going to delete it
 }
 
 void ARM::Reset()
@@ -752,19 +742,19 @@ void ARMv5::ExecuteJIT()
         u32 instrAddr = R[15] - ((CPSR&0x20)?2:4);
 
         if ((instrAddr < FastBlockLookupStart || instrAddr >= (FastBlockLookupStart + FastBlockLookupSize))
-            && !ARMJIT::SetupExecutableRegion(0, instrAddr, FastBlockLookup, FastBlockLookupStart, FastBlockLookupSize))
+            && !JIT.SetupExecutableRegion(0, instrAddr, FastBlockLookup, FastBlockLookupStart, FastBlockLookupSize))
         {
             NDS::ARM9Timestamp = NDS::ARM9Target;
             Log(LogLevel::Error, "ARMv5 PC in non executable region %08X\n", R[15]);
             return;
         }
 
-        ARMJIT::JitBlockEntry block = ARMJIT::LookUpBlock(0, FastBlockLookup,
+        ARMJIT::JitBlockEntry block = JIT.LookUpBlock(0, FastBlockLookup,
             instrAddr - FastBlockLookupStart, instrAddr);
         if (block)
             ARM_Dispatch(this, block);
         else
-            ARMJIT::CompileBlock(this);
+            JIT.CompileBlock(this);
 
         if (StopExecution)
         {
@@ -909,19 +899,19 @@ void ARMv4::ExecuteJIT()
         u32 instrAddr = R[15] - ((CPSR&0x20)?2:4);
 
         if ((instrAddr < FastBlockLookupStart || instrAddr >= (FastBlockLookupStart + FastBlockLookupSize))
-            && !ARMJIT::SetupExecutableRegion(1, instrAddr, FastBlockLookup, FastBlockLookupStart, FastBlockLookupSize))
+            && !JIT.SetupExecutableRegion(1, instrAddr, FastBlockLookup, FastBlockLookupStart, FastBlockLookupSize))
         {
             NDS::ARM7Timestamp = NDS::ARM7Target;
             Log(LogLevel::Error, "ARMv4 PC in non executable region %08X\n", R[15]);
             return;
         }
 
-        ARMJIT::JitBlockEntry block = ARMJIT::LookUpBlock(1, FastBlockLookup,
+        ARMJIT::JitBlockEntry block = JIT.LookUpBlock(1, FastBlockLookup,
             instrAddr - FastBlockLookupStart, instrAddr);
         if (block)
             ARM_Dispatch(this, block);
         else
-            ARMJIT::CompileBlock(this);
+            JIT.CompileBlock(this);
 
         if (StopExecution)
         {

src/ARM.h
@@ -42,10 +42,16 @@ enum
 const u32 ITCMPhysicalSize = 0x8000;
 const u32 DTCMPhysicalSize = 0x4000;
 
+namespace ARMJIT
+{
+class ARMJIT;
+}
 namespace Melon
 {
 class GPU;
 }
+
+class ARMJIT_Memory;
 
 class ARM
 #ifdef GDBSTUB_ENABLED
@@ -53,7 +59,7 @@ class ARM
 #endif
 {
 public:
-    ARM(u32 num, Melon::GPU& gpu);
+    ARM(u32 num, ARMJIT::ARMJIT& jit, Melon::GPU& gpu);
     virtual ~ARM(); // destroy shit
 
     virtual void Reset();
@@ -179,11 +185,12 @@ public:
     u64* FastBlockLookup;
 #endif
 
-    static u32 ConditionTable[16];
+    static const u32 ConditionTable[16];
 #ifdef GDBSTUB_ENABLED
     Gdb::GdbStub GdbStub;
 #endif
 
+    ARMJIT::ARMJIT& JIT;
 protected:
     u8 (*BusRead8)(u32 addr);
     u16 (*BusRead16)(u32 addr);
@@ -221,7 +228,7 @@ private:
 class ARMv5 : public ARM
 {
 public:
-    ARMv5(Melon::GPU& gpu);
+    ARMv5(ARMJIT::ARMJIT& jit, Melon::GPU& gpu);
     ~ARMv5();
 
     void Reset() override;
@@ -365,7 +372,7 @@ public:
 class ARMv4 : public ARM
 {
 public:
-    ARMv4(Melon::GPU& gpu);
+    ARMv4(ARMJIT::ARMJIT& jit, Melon::GPU& gpu);
 
     void Reset() override;
 

ag-advania/melonPrimeDS

commit 47af7247fdc9661e49dfcf5604d0bf00992ecc81

JIT: fastmem: fix Android predefined macro
Correct the Android macro to __ANDROID__ instead of ANDROID.

Closes: https://github.com/melonDS-emu/melonDS/issues/2706
Fixes: 544fefa27f69 ("Refactor the JIT to be object-oriented (#1879)")

src/ARMJIT_Memory.h
@@ -190,7 +190,7 @@ private:
     static void SigsegvHandler(int sig, siginfo_t* info, void* rawContext);
     int MemoryFile = -1;
 #endif
-#ifdef ANDROID
+#if defined(__ANDROID__)
     Platform::DynamicLibrary* Libandroid = nullptr;
 #endif
     u8 MappingStatus9[1 << (32-12)] {};